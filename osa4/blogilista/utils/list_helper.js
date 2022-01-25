4c8bf2806b28bcc9f410d8271ce87e001f1f01fa
Eppu Peltonen <eppu.peltonen@icloud.com>
2022-01-25 10:00:27 +0200

    4.4 totalLikes apufunktio ja testit

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,15 @@
-const dummy = (blogs) => {
-  return 1
+const totalLikes = (blogs) => {
+  const likes = blogs.map(x => x.likes)
+  if (likes.length === 0) {
+    return 0
+  }  else {
+    const totalLikes = likes.reduce((sum, like) => {
+      return sum + like
+    })
+    return totalLikes
+  }
 }
 
 module.exports = {
-  dummy
+  totalLikes
 }
