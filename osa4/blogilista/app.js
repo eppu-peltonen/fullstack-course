9f4055524c1c8f6c33c1dcf7151a22533dabf09b
Eppu Peltonen <eppu.peltonen@icloud.com>
2022-01-25 09:30:41 +0200

    4.3 test environment and eslint

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -17,14 +17,14 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB: ', error.message)
   })
 
-  app.use(cors())
-  app.use(express.static('build'))
-  app.use(express.json())
-  app.use(middleware.requestLogger)
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
 
-  app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter)
 
-  app.use(middleware.unknownEndpoint)
+app.use(middleware.unknownEndpoint)
 
-  module.exports = app
+module.exports = app
 
