c0a4a0f1fc83ee6828af7fde7ef3118f1c94949a
Eppu Peltonen <eppu.peltonen@icloud.com>
2022-01-31 11:14:11 +0200

    4.17 blogin ja käyttäjän yhteys

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({})
@@ -7,8 +8,26 @@ blogsRouter.get('/', async (req, res) => {
 })
 
 blogsRouter.post('/', async (req, res) => {
-  const blog = new Blog(req.body)
+  const body = req.body
+
+  const user = await User.findById(body.userId)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+
+  if (blog.title === undefined) {
+    return res.status(400).json({error: 'title missing'})
+  } else if (blog.url === undefined) {
+    return res.status(400).json({error: 'url missing'})
+  }
+  
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   res.json(savedBlog.toJSON())
 })
 
