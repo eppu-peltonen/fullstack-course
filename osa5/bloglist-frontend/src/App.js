9a5e9100296dba6ba9f7cd9bffbbadeaf86b13e0
Eppu Peltonen <eppu.peltonen@icloud.com>
2022-02-01 13:27:47 +0200

    5.1 frontend kirjautuminen

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch(exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+              type="text"
+              value={username}
+              name="username"
+              onChange={({target}) => setUsername(target.value)}
+            />
+        </div>
+        <div>
+          password
+            <input
+              type="password"
+              value={password}
+              name="password"
+              onChange={({target}) => setPassword(target.value)}
+            />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
+  const blogList = () => (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+
+  return (
+    <div>
+      <Notification message={errorMessage} />
+      {
+        user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in</p>
+          {blogList()}
+        </div>
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
+const getAll = async () => {
+  const config = {
+    headers: {Authorization: token},
+  }
+  const response = await axios.get(baseUrl, config)
+  return response.data
+}
+
+const exportObject = {
+  getAll, setToken
+}
+
+export default exportObject
